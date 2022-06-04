b496cc3944d3e6c2af399673257d97332cf3871f
Valentin <valentin_00794@bk.ru>
2022-06-04 22:00:50 +0300

    Устранены замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно (Источник https://learn.javascript.ru/number)
-function randomNumber(min, max, decimal) {
-  if (min< 0 || max <0 || decimal>20) {
-    return undefined;
-  } else {
-    if (!decimal || decimal===0){
-      const rand = min + Math.random() * (max + 1 - min);
-      return Math.floor(rand);
-    }
+function getRandomNumber(min, max, decimal) {
+  if (min> 0 && max >0 && decimal<20) {
     return (min + Math.random() * (max - min)).toFixed(decimal);
   }
+  if (!decimal || decimal===0){
+    const rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
 }
-randomNumber(0, 12,20);
-
+getRandomNumber(-1, 1,20);

827b949aa6c33136470e6cc296d5e3ead5c6625e
Valentin <valentin_00794@bk.ru>
2022-06-19 12:02:24 +0300

    Валидация

--- js/form.js
+++ js/form.js
@@ -21,4 +21,25 @@ function pageToActive() {
   disOrEnableFormElements(adForm, false);
   disOrEnableFormElements(mapForm, false);
 }
+
+const form = document.querySelector('.ad-form');
+
+const pristine = new Pristine(form, {
+
+});
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const isValid = pristine.validate();
+  if (isValid) {
+    console.log('Можно отправлять');
+  } else {
+    console.log('Форма невалидна');
+  }
+});
+
+
+
+
+
 export { pageToNotActive, pageToActive };

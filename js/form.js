34c5754e4203feed4a023ef331645e6876f978cc
Valentin <valentin_00794@bk.ru>
2022-06-20 16:46:39 +0300

    ДЗ Правда или действие 1

--- js/form.js
+++ js/form.js
@@ -22,24 +22,46 @@ function pageToActive() {
   disOrEnableFormElements(mapForm, false);
 }
 
+const TITLE_LENGTH_RANGE = {
+  min: 30,
+  max: 100,
+};
 const form = document.querySelector('.ad-form');
-
 const pristine = new Pristine(form, {
-
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'ad-form__error-text',
 });
+function validateTitle (value) {
+  return value.length >= TITLE_LENGTH_RANGE.min && value.length <= TITLE_LENGTH_RANGE.max;
+}
+pristine.addValidator();
+pristine.addValidator(
+  form.querySelector('#title'),
+  validateTitle,
+  'от 30 до 100 символов'
+);
+function validatePrice (value) {
+  return  value <= 100000;
+}
+pristine.addValidator(
+  form.querySelector('#price'),
+  validatePrice,
+  'не более 100000 руб.'
+);
+function validateRoomsGuests () {
+  const roomFields = form.querySelector('#room_number');
+  const guestsFields = form.querySelector('#capacity');
+  return (guestsFields.value === '0' && roomFields.value === '100') ||
+  (guestsFields.value <= roomFields.value && roomFields.value !== '100' && guestsFields.value !== '0');
+}
+pristine.addValidator(
+  form.querySelector('#capacity'),
+  validateRoomsGuests,
+  'Гостям тесно'
+);
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  const isValid = pristine.validate();
-  if (isValid) {
-    console.log('Можно отправлять');
-  } else {
-    console.log('Форма невалидна');
-  }
+  pristine.validate();
 });
-
-
-
-
-
 export { pageToNotActive, pageToActive };
